974f74f3842a162fbfe43ca70ce7e0789c1de450
cameron-yee <yee.camero23@gmail.com>
2018-10-17 21:14:36 -0600

    Changed some page content

--- src/pages/about.js
+++ src/pages/about.js
@@ -5,9 +5,9 @@ import Layout from '../components/layout'
 
 const SecondPage = () => (
   <Layout>
-    <h1>Hi from the second page</h1>
-    <p>Welcome to page 2</p>
-    <Link to="/">Go back to the homepage</Link>
+    <h1>About Cameron</h1>
+    <p>Cameron is ...</p>
+    <Link to="/">Homepage</Link>
   </Layout>
 )
 
