0eeaa51b7a64ffe5e6e833759eecbdc6a615a891
cameron-yee <yee.camero23@gmail.com>
2018-10-25 14:11:01 -0600

    Starting over

--- src/components/sleepform.js
+++ src/components/sleepform.js
@@ -2,7 +2,7 @@ import React from 'react'
 import 'bulma/bulma.sass'
 
 const SleepForm = (props) => (
-  <form name="sleep" method="POST" data-netlify="true" data-netlify-honeypot="bot-field">
+  <form name="sleep" method="POST" action="/success" data-netlify="true" data-netlify-honeypot="bot-field">
     <input type="hidden" name="bot-field" />
     <div className="columns is-multiline">
       <div className="column is-half">
